67cdb6552fb28cc3d5f5a0fec858a34cc806a18a
Keshav5173 <bhardwajkeshav5173@gmail.com>
2026-09-23 10:31:22 +0530

    setup completed for multer and cloudinary

---
+++ src/utlis/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET_KEY 
+});
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try{
+        if(!localFilePath){
+            return null;
+        }
+        const uploadedData = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("File uploaded on cloudinary", uploadedData);
+        return uploadedData;
+    }
+    catch(err){
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export { uploadOnCloudinary };
